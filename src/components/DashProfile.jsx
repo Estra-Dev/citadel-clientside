04a6f40ad90d0838fd3a77608a402c5f14163327
Estra-Dev <dominikolyson@gmail.com>
2024-02-22 17:42:37 +0100

    complete user image upload functionality

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -1,19 +1,115 @@
-import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
-import {useSelector} from 'react-redux'
+import { Alert, Button, TextInput } from 'flowbite-react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import { app } from '../firebase'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const DashProfile = () => {
 
-  const {currentUser} = useSelector(state => state.user)
+  const { currentUser } = useSelector(state => state.user)
+  const [imageFile, setImageFile] = useState(null)
+  const [imageFileUrl, setImageFileUrl] = useState(null)
+  const [imageFileUploadingProgress, setImageFileUploadingProgress] = useState(null)
+  const [imageFileUploadingError, setImageFileUploadingError] = useState(null)
+
+  // To enable us pick image when we click on the profile image
+  const filePickerRef = useRef()
+
+  const handleImageChange = (ev) => {
+
+    const file = ev.target.files[0]
+
+    if (file) {
+      setImageFile(file)
+      setImageFileUrl(URL.createObjectURL(file))
+    }
+
+  }
+
+  // anytime we have a new image, we have to upload it automatically by using useEffect()
+  useEffect(() => {
+    if (imageFile) {
+      uploadImg()
+    }
+  }, [imageFile])
+
+  const uploadImg = async () => {
+    // If I need the code again
+    //   service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read;
+    //       allow write: if
+    //       request.resource.size < 14 * 1024 * 1024 && 
+    //       request.resource.contentType.matches('image/.*')
+    //     }
+    //   }
+    // }
+    setImageFileUploadingProgress(null)
+    const storage = getStorage(app)
+    // to make the image name unique we add the new Date().getTime()
+    const fileName = new Date().getTime() + imageFile.name
+    const storageRef = ref(storage, fileName)
+
+    // uploadtask is a method we want to use to upload our image and get the information while it is uploading e.g: the amount of byte that is uploading
+    const uploadTask = uploadBytesResumable(storageRef, imageFile)
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = 
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        setImageFileUploadingProgress(progress.toFixed(0))
+      },
+      (error) => {
+        setImageFileUploadingError("Could not Upload Image")
+        setImageFileUploadingProgress(null)
+        setImageFile(null)
+        setImageFileUrl(null)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+          setImageFileUrl(downloadUrl)
+        })
+      }
+    )
+
+  }
 
   return (
     <div className=' max-w-lg mx-auto p-3 w-full'>
       <h1 className=' my-7 text-center font-semibold text-3xl'>Profile</h1>
 
       <form className=' flex flex-col gap-4'>
-        <div className=" w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden">
-          <img src={currentUser.rest.photoUrl} alt="profile img" className=' rounded-full w-full h-full border-8 border-[lightgray] object-cover' />
+        <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
+        <div className=" w-32 h-32 self-center cursor-pointer shadow-md rounded-full overflow-hidden relative" onClick={() => filePickerRef.current.click()}>
+          {imageFileUploadingProgress && (
+            <CircularProgressbar
+              value={imageFileUploadingProgress || 0}
+              text={`${imageFileUploadingProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${imageFileUploadingProgress / 100})`
+                }
+              }}
+            />
+          )}
+          <img src={imageFileUrl || currentUser.rest.photoUrl} alt="profile img" className={` rounded-full w-full h-full border-8 border-[lightgray] object-cover ${imageFileUploadingProgress && imageFileUploadingProgress < 100 && 'opacity-60'}`} />
         </div>
+        {
+          imageFileUploadingError && (
+            <Alert color={'failure'}>{ imageFileUploadingError }</Alert>
+          )
+        }
         <TextInput type='text' name='firstname' placeholder='Firstname' defaultValue={currentUser.rest.firstname} />
         <TextInput type='text' name='lastname' placeholder='Lastname' defaultValue={currentUser.rest.lastname} />
         <TextInput type='email' name='name' placeholder='Email' defaultValue={currentUser.rest.email} />
