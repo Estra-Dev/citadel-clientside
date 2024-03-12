68d2186ddc7dac4827629da7e4f1e9e26f25f4ff
Estra-Dev <dominikolyson@gmail.com>
2024-03-12 18:45:47 +0100

    add delete functionality to admin user

--- src/components/DashUser.jsx
+++ src/components/DashUser.jsx
@@ -54,21 +54,21 @@ const DashUser = () => {
     }
   }
 
-  // const handleDeleteUser = async () => {
-  //   setShowModal(false)
-  //   try {
-  //     const res = await axios.delete(`http://localhost:3000/user/deleteuser/${userIdToDelete}/${currentUser.rest._id || currentUser._id}`, {
-  //       withCredentials: true
-  //     })
-  //     if (res.status !== 200) {
-  //       console.log(res.response.data.message)
-  //     } else {
-  //       setUserPosts((prev) => prev.filter((post) => post._id !== postToDelete))
-  //     }
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
+  const handleDeleteUser = async () => {
+    setShowModal(false)
+    try {
+      const res = await axios.delete(`http://localhost:3000/user/delete/${userIdToDelete}`, {
+        withCredentials: true
+      })
+      if (res.status === 202) {
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete))
+      } else {
+        console.log(res)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div className=' table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -132,7 +132,7 @@ const DashUser = () => {
             <BsExclamationCircle className=' h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
             <h3 className=' text-lg text-gray-500 mb-5 dark:text-gray-400'>Are you sure you want to delete this user?</h3>
             <div className=" flex justify-center gap-4">
-              <Button color='failure'>Yes, I'm Sure</Button>
+              <Button color='failure' onClick={handleDeleteUser}>Yes, I'm Sure</Button>
               <Button onClick={() => setShowModal(false)} color='gray'>No, Cancel</Button>
             </div>
           </div>
