542b0cf3073a4380de1336130b16b4111dbc6d6c
Estra-Dev <dominikolyson@gmail.com>
2024-03-27 18:03:56 +0100

    upto date

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className=' min-h-screen flex items-center justify-center'>
       <div className=" max-w-2xl mx-auto p-3 text-center">
         <div className="">
-          <h1 className=' text-3xl font-semibold text-center my-7'>About Our App</h1>
+          <h1 className=' text-3xl font-semibold text-center my-7'>About Our App.</h1>
           <div className=" text-md text-gray-500 flex flex-col gap-6">
             <p>Citadel Of Treasure Ministry is a family of Gods people with the mandate to Unearth Hidden treasures in people's lives, to Redefine destinies and to Depopulate the kingdom of Darkness.</p>
             <p>Citadel Of Treasure Ministry is a family of Gods people with the mandate to Unearth Hidden treasures in people's lives, to Redefine destinies and to Depopulate the kingdom of Darkness.</p>
